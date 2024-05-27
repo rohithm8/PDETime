f0bec2bfbdefbdfa07222a6f49512c1b4fc83041
Rohith Manikonda <rohith.manikonda@keble.ox.ac.uk>
2024-05-27 23:21:30 +0100

    Update PDETimeDataset import in train.py

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import torch.utils.data
 import pandas as pd
 import numpy as np
 from src.PDETime.models.PDETime import PDETime, PDETimeLoss
-from src.PDETime.preprocess.dataloader import ETTm1Dataset, make_temporal_features
+from src.PDETime.preprocess.dataloader import PDETimeDataset, make_temporal_features
 
 if torch.cuda.is_available():
     device = torch.device('cuda')
@@ -18,7 +18,7 @@ horizon=96
 lookback=horizon*5
 
 model = PDETime(
-    spatial_dim=7,
+    dimension=7,
     temporal_features=5,
     temporal_latent_features=32,
     lookback=lookback,
@@ -57,7 +57,7 @@ print("Test Length:", len(test_data))
 print("Validation Length:", len(val_data))
 
 
-dataset = ETTm1Dataset(make_temporal_features(train_data))
+dataset = PDETimeDataset(make_temporal_features(train_data))
 dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True, generator=torch.Generator(device='cuda'))
 
 
@@ -79,7 +79,6 @@ torch.save(model, 'model.pt')
 # In[]:
 x_tau_0 = outputs[:,lookback-1,:].unsqueeze(-2).repeat(1, horizon, 1)
 x_tau_0 = torch.cat((torch.zeros_like(outputs[:,:lookback,:]), x_tau_0), dim=-2)
-x_tau_0.shape
 
 labelss = labels.detach().cpu().numpy()
 outputsss = (outputs + x_tau_0).detach().cpu().numpy()
@@ -87,7 +86,7 @@ import matplotlib.pyplot as plt
 
 # In[]:
 
-batch_no = 0
+batch_no = 2
 plt.plot(labelss[batch_no,:,-1], label='ground truth')
 plt.plot(outputsss[batch_no,:,-1], label='preds')
 plt.axvline(x=lookback, color='r')
