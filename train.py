3527badfe255591c834ae3f07ef7fa5ca205193d
Rohith Manikonda <rohith.manikonda@keble.ox.ac.uk>
2024-05-29 16:34:19 +0100

    Reflect changes in dataloader

--- train.py
+++ train.py
@@ -6,7 +6,7 @@ import torch.utils.data
 import pandas as pd
 import numpy as np
 from src.PDETime.models.PDETime import PDETime, PDETimeLoss
-from src.PDETime.preprocess.dataloader import PDETimeDataset, make_temporal_features
+from src.PDETime.preprocess.dataloader import PDETimeDataset
 
 if torch.cuda.is_available():
     device = torch.device('cuda')
@@ -19,7 +19,7 @@ lookback=horizon*5
 
 model = PDETime(
     dimension=7,
-    temporal_features=5,
+    temporal_features=7,
     temporal_latent_features=32,
     lookback=lookback,
     horizon=horizon,
@@ -39,43 +39,35 @@ criterion = PDETimeLoss(torch.nn.SmoothL1Loss(), lookback, horizon)
 optimizer = torch.optim.Adam(model.parameters(), lr=1e-4)
 
 df = pd.read_csv('data/ETTm1.csv')
-total_length = len(df)
-train_ratio = 0.7
-test_ratio = 0.2
-val_ratio = 0.1
-train_length = int(total_length * train_ratio)
-test_length = int(total_length * test_ratio)
-val_length = total_length - train_length - test_length
 
-
-train_data = df[:train_length]
-test_data = df[train_length:train_length+test_length]
-val_data = df[train_length+test_length:]
+train_data, test_data, val_data = PDETimeDataset.train_test_val_split(df, horizon=horizon, lookback=lookback, val_ratio=0.1, test_ratio=0.2)
 
 print("Train Length:", len(train_data))
 print("Test Length:", len(test_data))
 print("Validation Length:", len(val_data))
 
 
-dataset = PDETimeDataset(make_temporal_features(train_data))
-dataloader = torch.utils.data.DataLoader(dataset, batch_size=32, shuffle=True, generator=torch.Generator(device='cuda'))
 
+dataloader = torch.utils.data.DataLoader(train_data, batch_size=32, shuffle=True, generator=torch.Generator(device='cuda'))
 
 # In[]:
 
 model.train()
-for epoch in range(1):
+for epoch in range(10):
+    epoch_loss = 0
     for i, data in enumerate(tqdm(dataloader)):
         data = [data.to('cuda', dtype=torch.float) for data in data]
         x, t, tau, labels = data
         optimizer.zero_grad()
         outputs = model(x, t, tau)
         loss = criterion(outputs, labels.float())
+        epoch_loss += loss
         loss.backward()
         optimizer.step()
-    print("epoch:", epoch, "    loss:", loss.item())
+    epoch_loss /= len(dataloader)
+    print("epoch:", epoch, "    training loss:", epoch_loss.item())
 model.eval()
-torch.save(model, 'model.pt')
+# torch.save(model, 'model.pt')
 # In[]:
 x_tau_0 = outputs[:,lookback-1,:].unsqueeze(-2).repeat(1, horizon, 1)
 x_tau_0 = torch.cat((torch.zeros_like(outputs[:,:lookback,:]), x_tau_0), dim=-2)
